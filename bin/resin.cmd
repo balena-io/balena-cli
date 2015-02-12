fe9e76cdffde2c77b205021a95ae8f10cfe58be8
Juan Cruz Viotti <juanchiviotti@gmail.com>
2015-02-12 15:20:09 -0400

    Comment source of Windows arch snippet

--- bin/resin.cmd
+++ bin/resin.cmd
@@ -1,5 +1,6 @@
 @echo off
 
+:: http://stackoverflow.com/questions/12322308/batch-file-to-check-64bit-or-32bit-os
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set ARCH=x86 || set ARCH=x64
 
 set NODE_PATH="%~dp0\bin\node\node-win32-%ARCH%.exe"
