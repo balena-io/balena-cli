188d6213e3dd4e17610da3ba38e9000165510649
Juan Cruz Viotti <juanchiviotti@gmail.com>
2015-02-13 15:35:28 -0400

    Only reference bundle node version in package.json

--- bin/install-node.js
+++ bin/install-node.js
@@ -1,5 +1,8 @@
 var binary = require('node-binary');
-var nodeVersion = 'v0.12.0';
+var fs = require('fs');
+var path = require('path');
+var nodeVersion = require('../package.json').bundled_engine;
+var destination = './bin/node';
 
 console.log('Downloading node-' + nodeVersion + '-' + process.platform + '-' + process.arch);
 
@@ -7,11 +10,14 @@ binary.download({
 	os: process.platform,
 	arch: process.arch,
 	version: nodeVersion
-}, './bin/node', function(error, binaryPath) {
+}, destination, function(error, binaryPath) {
 	if(error) {
 		console.error(error.message);
 		process.exit(1);
 	}
 
-	console.log('NodeJS was downloaded to ' + binaryPath);
+	var output = path.join(destination, 'node-' + process.platform + '-' + process.arch);
+	fs.renameSync(binaryPath, output);
+
+	console.log('NodeJS downloaded to ' + output);
 });
