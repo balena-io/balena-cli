782c92885d137c696439673ababd8eb16151b585
Juan Cruz Viotti <juanchiviotti@gmail.com>
2015-03-24 08:44:02 -0400

    Use child_process to trigger self update
    Using the NPM API led to weird issues.
    
    See https://github.com/npm/npm/issues/7723.

--- build/npm.js
+++ build/npm.js
@@ -7,31 +7,6 @@
 
   _ = require('lodash-contrib');
 
-  exports.update = function(name, callback) {
-    return async.waterfall([
-      function(callback) {
-        var options;
-        options = {
-          loglevel: 'silent',
-          global: true
-        };
-        return npm.load(options, _.unary(callback));
-      }, function(callback) {
-        return npm.commands.update([name], function(error, data) {
-          return callback(error, data);
-        });
-      }, function(data, callback) {
-        var error, newVersion;
-        if (_.isEmpty(data)) {
-          error = new Error('You are already running the latest version');
-          return callback(error);
-        }
-        newVersion = _.last(_.first(_.last(data)).split('@'));
-        return callback(null, newVersion);
-      }
-    ], callback);
-  };
-
   exports.getLatestVersion = function(name, callback) {
     return async.waterfall([
       function(callback) {
