ef33156de78eae84892d32ebf0115bf22d00a594
Juan Cruz Viotti <juanchiviotti@gmail.com>
2015-06-10 12:34:42 -0400

    Implement config command

--- build/actions/info.js
+++ build/actions/info.js
@@ -1,5 +1,7 @@
 (function() {
-  var packageJSON;
+  var packageJSON, settings;
+
+  settings = require('resin-settings-client');
 
   packageJSON = require('../../package.json');
 
@@ -9,6 +11,21 @@
     help: 'Display the Resin CLI version.',
     action: function(params, options, done) {
       console.log(packageJSON.version);
+      return done();
+    }
+  };
+
+  exports.config = {
+    signature: 'config',
+    description: 'see your current configuration',
+    help: 'See your current Resin CLI configuration.\n\nConfiguration lives in $HOME/.resin/config.',
+    action: function(params, options, done) {
+      var key, ref, value;
+      ref = settings.get();
+      for (key in ref) {
+        value = ref[key];
+        console.log(key + ": " + value);
+      }
       return done();
     }
   };
