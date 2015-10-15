b2425d2c0e3d7f346e7f8b091bb018faf82a8354
Juan Cruz Viotti <juanchiviotti@gmail.com>
2015-10-15 08:48:34 -0400

    Take device type as an option in os initialize

--- build/actions/device.js
+++ build/actions/device.js
@@ -154,7 +154,7 @@
         return Promise.using(download(), function(temporalPath) {
           return capitano.runAsync("device register " + application.app_name).then(resin.models.device.get).tap(function(device) {
             return capitano.runAsync("os configure " + temporalPath + " " + device.uuid).then(function() {
-              return helpers.sudo(['os', 'initialize', temporalPath, application.device_type]);
+              return helpers.sudo(['os', 'initialize', temporalPath, '--type', application.device_type]);
             });
           });
         }).then(function(device) {
