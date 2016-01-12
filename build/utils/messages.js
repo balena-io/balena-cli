78ab2af8baefd7a5aa659b4b652876ead9ce71fb
Juan Cruz Viotti <jviottidc@gmail.com>
2016-01-12 10:39:29 -0400

    Print verbose help in resin help command

---
+++ build/utils/messages.js
@@ -0,0 +1,6 @@
+(function() {
+  exports.gettingStarted = 'Run the following command to get a device started with Resin.io\n\n  $ resin quickstart';
+
+  exports.reachingOut = 'If you need help, or just want to say hi, don\'t hesitate in reaching out at:\n\n  GitHub: https://github.com/resin-io/resin-cli/issues/new\n  Gitter: https://gitter.im/resin-io/chat';
+
+}).call(this);
---
+++ lib/utils/messages.coffee
@@ -0,0 +1,12 @@
+exports.gettingStarted = '''
+	Run the following command to get a device started with Resin.io
+
+	  $ resin quickstart
+'''
+
+exports.reachingOut = '''
+	If you need help, or just want to say hi, don't hesitate in reaching out at:
+
+	  GitHub: https://github.com/resin-io/resin-cli/issues/new
+	  Gitter: https://gitter.im/resin-io/chat
+'''
