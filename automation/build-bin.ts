3b52f7ba4e28365f839d972c232e256ee4261f17
Tim Perry <pimterry@gmail.com>
2017-12-13 15:08:36 +0100

    Set up a script to automate builds, and support native extensions

---
+++ automation/build-bin.ts
@@ -0,0 +1,34 @@
+import * as path from 'path';
+import * as fs from 'fs-extra';
+import * as FileHound from 'filehound';
+import { exec as execPkg } from 'pkg';
+
+const ROOT = path.join(__dirname, '..');
+
+console.log('Building package...\n');
+
+execPkg([
+	'--target', 'host',
+	'--output', 'build-bin/resin',
+	'package.json'
+]).then(() => fs.copy(
+	path.join(ROOT, 'node_modules', 'opn', 'xdg-open'),
+	path.join(ROOT, 'build-bin', 'xdg-open')
+)).then(() => {
+	return FileHound.create()
+		.paths(path.join(ROOT, 'node_modules'))
+		.ext(['node', 'dll'])
+		.find();
+}).then((nativeExtensions: string[]) => {
+	console.log(`\nCopying to build-bin:\n${nativeExtensions.join('\n')}`);
+
+	return nativeExtensions.map((extPath) => {
+		return fs.copy(
+			extPath,
+			extPath.replace(
+				path.join(ROOT, 'node_modules'),
+				path.join(ROOT, 'build-bin')
+			)
+		);
+	});
+});
