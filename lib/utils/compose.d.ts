fd7bf2f5236cdff511a5352d7dd9cc6050ffeac7
Cameron Diver <cameron@resin.io>
2018-10-15 12:31:10 +0100

    compose: Add compose typings
    Change-type: minor
    Signed-off-by: Cameron Diver <cameron@resin.io>

--- lib/utils/compose.d.ts
+++ lib/utils/compose.d.ts
@@ -1,3 +1,32 @@
+import * as Bluebird from 'bluebird';
 import * as Stream from 'stream';
+import { Composition } from 'resin-compose-parse';
+import Logger = require('./logger');
+
+interface Image {
+	context: string;
+	tag: string;
+}
+
+interface Descriptor {
+	image: Image | string;
+	serviceName: string;
+}
+
+export function resolveProject(projectRoot: string): Bluebird<string>;
+
+export interface ComposeProject {
+	path: string;
+	name: string;
+	composition: Composition;
+	descriptors: Descriptor[];
+}
+
+export function loadProject(
+	logger: Logger,
+	projectPath: string,
+	projectName: string,
+	image?: string,
+): Bluebird<ComposeProject>;
 
 export function tarDirectory(source: string): Promise<Stream.Readable>;
