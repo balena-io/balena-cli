02dcff5b67223909f5cd465a60498802246c5ea7
Paulo Castro <paulo@balena.io>
2019-11-21 15:31:45 +0000

    Fix 'balena help join' docs re moving devices between apps on the same server
    Resolves: #1429
    Change-type: patch
    Signed-off-by: Paulo Castro <paulo@balena.io>

--- lib/actions/join.ts
+++ lib/actions/join.ts
@@ -36,7 +36,8 @@ export const join: CommandDefinition<Args, Options> = {
 		where you perform end-to-end tests and then move it to balenaCloud when it's
 		ready for production.
 
-		Moving a device between applications on the same server is not supported.
+		To move a device between applications on the same server, use the
+		\`balena device move\` command instead of \`balena join\`.
 
 		If you don't specify a device hostname or IP, this command will automatically
 		scan the local network for balenaOS devices and prompt you to select one
