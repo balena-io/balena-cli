b701151769a1bf8bd4a161419d84ddf173c1893a
Kyle Harding <kyle@balena.io>
2021-09-10 13:32:12 +0000

    build/deploy: Update QEMU to 6.0.0 for emulated builds
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- lib/utils/qemu.ts
+++ lib/utils/qemu.ts
@@ -21,7 +21,7 @@ import { ExpectedError } from '../errors';
 import { getBalenaSdk, stripIndent } from './lazy';
 import Logger = require('./logger');
 
-export const QEMU_VERSION = 'v5.2.0+balena4';
+export const QEMU_VERSION = 'v6.0.0+balena1';
 export const QEMU_BIN_NAME = 'qemu-execve';
 
 export function qemuPathInContext(context: string) {
