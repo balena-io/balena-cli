7564d95f82e21b4f6953b7febb7ed9f6677f515c
Paulo Castro <paulo@balena.io>
2021-12-19 22:40:26 +0000

    devices supported: Remove deprecated '--verbose' and '--discontinued' options
    Change-type: major

--- lib/commands/devices/supported.ts
+++ lib/commands/devices/supported.ts
@@ -21,33 +21,18 @@ import Command from '../../command';
 import * as cf from '../../utils/common-flags';
 import { getBalenaSdk, getVisuals, stripIndent } from '../../utils/lazy';
 import { CommandHelp } from '../../utils/oclif-utils';
-import { isV13 } from '../../utils/version';
 
 interface FlagsDef {
-	discontinued: boolean;
 	help: void;
 	json?: boolean;
-	verbose?: boolean;
 }
-
-const deprecatedInfo = isV13()
-	? ''
-	: `
-The --verbose option may add extra columns/fields to the output. Currently
-this includes the "STATE" column which is DEPRECATED and whose values are one
-of 'new', 'released' or 'discontinued'. However, 'discontinued' device types
-are only listed if the '--discontinued' option is also used, and this option
-is also DEPRECATED.
-`
-			.split('\n')
-			.join(`\n\t\t`);
 
 export default class DevicesSupportedCmd extends Command {
 	public static description = stripIndent`
 		List the supported device types (like 'raspberrypi3' or 'intel-nuc').
 
 		List the supported device types (like 'raspberrypi3' or 'intel-nuc').
-		${deprecatedInfo}
+
 		The --json option is recommended when scripting the output of this command,
 		because the JSON format is less likely to change and it better represents data
 		types like lists and empty strings (for example, the ALIASES column contains a
@@ -56,8 +41,7 @@ export default class DevicesSupportedCmd extends Command {
 `;
 	public static examples = [
 		'$ balena devices supported',
-		'$ balena devices supported --verbose',
-		'$ balena devices supported -vj',
+		'$ balena devices supported --json',
 	];
 
 	public static usage = (
@@ -66,21 +50,10 @@ export default class DevicesSupportedCmd extends Command {
 	).trim();
 
 	public static flags: flags.Input<FlagsDef> = {
-		discontinued: flags.boolean({
-			description: isV13()
-				? 'No effect (DEPRECATED)'
-				: 'include "discontinued" device types (DEPRECATED)',
-		}),
 		help: cf.help,
 		json: flags.boolean({
 			char: 'j',
 			description: 'produce JSON output instead of tabular output',
-		}),
-		verbose: flags.boolean({
-			char: 'v',
-			description: isV13()
-				? 'No effect (DEPRECATED)'
-				: 'add extra columns in the tabular output (DEPRECATED)',
 		}),
 	};
 
@@ -95,53 +68,28 @@ export default class DevicesSupportedCmd extends Command {
 		]);
 		const dtsBySlug = _.keyBy(dts, (dt) => dt.slug);
 		const configDTsBySlug = _.keyBy(configDTs, (dt) => dt.slug);
-		const discontinuedDTs = isV13()
-			? []
-			: configDTs.filter((dt) => dt.state === 'DISCONTINUED');
-		const discontinuedDTsBySlug = _.keyBy(discontinuedDTs, (dt) => dt.slug);
-		// set of slugs from models.deviceType.getAllSupported() plus slugs of
-		// discontinued device types as per models.config.getDeviceTypes()
-		const slugsOfInterest = new Set([
-			...Object.keys(dtsBySlug),
-			...Object.keys(discontinuedDTsBySlug),
-		]);
 		interface DT {
 			slug: string;
 			aliases: string[];
 			arch: string;
-			state?: string; // to be removed in CLI v13
 			name: string;
 		}
 		let deviceTypes: DT[] = [];
-		for (const slug of slugsOfInterest) {
+		for (const slug of Object.keys(dtsBySlug)) {
 			const configDT: Partial<typeof configDTs[0]> =
 				configDTsBySlug[slug] || {};
-			if (configDT.state === 'DISCONTINUED' && !options.discontinued) {
-				continue;
-			}
-			const dt: Partial<typeof dts[0]> = dtsBySlug[slug] || {};
 			const aliases = (configDT.aliases || []).filter(
 				(alias) => alias !== slug,
 			);
+			const dt: Partial<typeof dts[0]> = dtsBySlug[slug] || {};
 			deviceTypes.push({
 				slug,
 				aliases: options.json ? aliases : [aliases.join(', ')],
-				arch:
-					(dt.is_of__cpu_architecture as any)?.[0]?.slug ||
-					configDT.arch ||
-					'n/a',
-				// 'BETA' renamed to 'NEW'
-				// https://www.flowdock.com/app/rulemotion/i-cli/threads/1svvyaf8FAZeSdG4dPJc4kHOvJU
-				state: isV13()
-					? undefined
-					: (configDT.state || 'NEW').replace('BETA', 'NEW'),
-				name: dt.name || configDT.name || 'N/A',
+				arch: (dt.is_of__cpu_architecture as any)?.[0]?.slug || 'n/a',
+				name: dt.name || 'N/A',
 			});
 		}
-		const fields =
-			options.verbose && !isV13()
-				? ['slug', 'aliases', 'arch', 'state', 'name']
-				: ['slug', 'aliases', 'arch', 'name'];
+		const fields = ['slug', 'aliases', 'arch', 'name'];
 		deviceTypes = _.sortBy(deviceTypes, fields);
 		if (options.json) {
 			console.log(JSON.stringify(deviceTypes, null, 4));
