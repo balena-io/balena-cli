1e6ab46ca3189cd2789fbd9d7e37506a8e922caa
Scott Lowe <scott@balena.io>
2021-12-23 15:56:50 +0100

    Add tips for removed commands
    Signed-off-by: Scott Lowe <scott@balena.io>

--- lib/preparser.ts
+++ lib/preparser.ts
@@ -131,6 +131,13 @@ Please use "balena ${alternative}" instead.`);
 		'local scan': [replaced, 'scan', 'v11.0.0'],
 		'local ssh': [replaced, 'ssh', 'v11.0.0'],
 		'local stop': [removed, stopAlternative, 'v11.0.0'],
+		app: [replaced, 'fleet', 'v13.0.0'],
+		apps: [replaced, 'fleets', 'v13.0.0'],
+		'app create': [replaced, 'fleet create', 'v13.0.0'],
+		'app purge': [replaced, 'fleet purge', 'v13.0.0'],
+		'app rename': [replaced, 'fleet rename', 'v13.0.0'],
+		'app restart': [replaced, 'fleet restart', 'v13.0.0'],
+		'app rm': [replaced, 'fleet rm', 'v13.0.0'],
 	};
 	let cmd: string | undefined;
 	if (argvSlice.length > 1) {
