b8e190cd1d318827bbf945a249f65cb068246da9
Otávio Jacobi <otaviojacobi@gmail.com>
2023-08-16 10:29:41 -0300

    Remove redundant envs documentation
    Change-type: patch

--- lib/commands/envs.ts
+++ lib/commands/envs.ts
@@ -93,7 +93,6 @@ export default class EnvsCmd extends Command {
 		'$ balena envs --fleet myorg/myfleet',
 		'$ balena envs --fleet MyFleet --json',
 		'$ balena envs --fleet MyFleet --service MyService',
-		'$ balena envs --fleet MyFleet --service MyService',
 		'$ balena envs --fleet MyFleet --config',
 		'$ balena envs --device 7cf02a6',
 		'$ balena envs --device 7cf02a6 --json',
