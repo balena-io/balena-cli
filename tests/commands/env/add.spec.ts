66b6eed57c000713beea2e28d4f56cdbfc2b0ea4
Thodoris Greasidis <thgreasi@gmail.com>
2023-10-20 13:06:26 +0300

    Add tests for empty & non-provided value for the `env add` command
    Change-type: patch

--- tests/commands/env/add.spec.ts
+++ tests/commands/env/add.spec.ts
@@ -23,6 +23,8 @@ import { runCommand } from '../../helpers';
 describe('balena env add', function () {
 	let api: BalenaAPIMock;
 
+	const fullUUID = 'f63fd7d7812c34c4c14ae023fdff05f5';
+
 	beforeEach(() => {
 		api = new BalenaAPIMock();
 		api.expectGetWhoAmI({ optional: true, persist: true });
@@ -35,7 +37,6 @@ describe('balena env add', function () {
 	});
 
 	it('should successfully add an environment variable', async () => {
-		const fullUUID = 'f63fd7d7812c34c4c14ae023fdff05f5';
 		api.expectGetDevice({ fullUUID });
 		api.expectGetConfigVars();
 		api.scope
@@ -43,6 +44,60 @@ describe('balena env add', function () {
 			.reply(200, 'OK');
 
 		const { out, err } = await runCommand(`env add TEST 1 -d ${fullUUID}`);
+
+		expect(out.join('')).to.equal('');
+		expect(err.join('')).to.equal('');
+	});
+
+	it('should reject adding an environment variable w/o specifying a value when the process environment does not have an env var with the same name defined', async () => {
+		// make sure that that there is no such env var defined atm
+		delete process.env.TEST_ENV_VAR_ADD_NO_VALUE_REJECTED;
+
+		const { out, err } = await runCommand(
+			`env add TEST_ENV_VAR_ADD_NO_VALUE_REJECTED -d ${fullUUID}`,
+		);
+
+		expect(out.join('')).to.equal('');
+		// Depending the context that the test was running , this was emiting either 1 or 2 '\n'
+		expect(err.join('')).to.be.oneOf([
+			'Value not found for environment variable: TEST_ENV_VAR_ADD_NO_VALUE_REJECTED\n',
+			'Value not found for environment variable: TEST_ENV_VAR_ADD_NO_VALUE_REJECTED\n\n',
+		]);
+	});
+
+	it('should successfully add an environment variable w/o specifying a value when the process environment has an env var with the same name defined', async () => {
+		api.expectGetDevice({ fullUUID });
+		api.expectGetConfigVars();
+		api.scope
+			.post(/^\/v\d+\/device_environment_variable($|\?)/)
+			.reply(200, 'OK');
+
+		process.env.TEST_ENV_VAR_ADD_NO_VALUE = '4';
+
+		const { out, err } = await runCommand(
+			`env add TEST_ENV_VAR_ADD_NO_VALUE -d ${fullUUID}`,
+		);
+
+		delete process.env.TEST_ENV_VAR_ADD_NO_VALUE;
+
+		expect(out.join('')).to.equal('');
+		expect(err.join('')).to.be.oneOf([
+			' ›   Warning: Using TEST_ENV_VAR_ADD_NO_VALUE=4 from CLI process environment\n',
+			// emitted when running on the windows instance
+			' »   Warning: Using TEST_ENV_VAR_ADD_NO_VALUE=4 from CLI process environment\n',
+		]);
+	});
+
+	it('should successfully add an environment variable w/ empty string as a value', async () => {
+		api.expectGetDevice({ fullUUID });
+		api.expectGetConfigVars();
+		api.scope
+			.post(/^\/v\d+\/device_environment_variable($|\?)/)
+			.reply(200, 'OK');
+
+		const { out, err } = await runCommand(
+			`env add TEST_EMPTY_STRING '' -d ${fullUUID}`,
+		);
 
 		expect(out.join('')).to.equal('');
 		expect(err.join('')).to.equal('');
