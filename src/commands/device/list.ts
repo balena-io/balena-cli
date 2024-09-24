23ce6261b39f688f87c718855be19866e0461700
myarmolinsky <yarmolinskymatthew@gmail.com>
2024-09-24 12:26:16 -0400

    Deprecate `devices` command in favor of `device list`
    Change-type: patch

--- src/commands/device/list.ts
+++ src/commands/device/list.ts
@@ -37,6 +37,7 @@ const devicesSelectFields = {
 
 export default class DeviceListCmd extends Command {
 	public static aliases = ['devices'];
+	public static deprecateAliases = true;
 
 	public static description = stripIndent`
 		List all devices.
