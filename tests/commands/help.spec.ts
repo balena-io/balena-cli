9b1eb57973e2b858e7c5042623bec78feaa9a0b4
myarmolinsky <yarmolinskymatthew@gmail.com>
2024-10-21 08:52:16 -0400

    Update help.spec per recent help output change

--- tests/commands/help.spec.ts
+++ tests/commands/help.spec.ts
@@ -27,65 +27,65 @@ $ balena [COMMAND] [OPTIONS]
 
 PRIMARY COMMANDS
   login                                  login to balena
-  push <applicationOrDevice>             start a remote build on the balenaCloud build servers or a local mode device
-  app <name>                             display information about a single application
-  device <uuid>                          show info about a single device
-  tunnel <deviceOrApplication>           tunnel local ports to your balenaOS device
-  preload <image>                        preload an app on a disk image (or Edison zip archive)
-  build [source]                         build a project locally
-  deploy <appName> [image]               deploy a single image or a multicontainer project to a balena application
-  join [deviceIpOrHostname]              move a local device to an application on another balena server
-  leave [deviceIpOrHostname]             remove a local device from its balena application
+  push                                   start a remote build on the balenaCloud build servers or a local mode device
+  app                                    display information about a single application
+  device                                 show info about a single device
+  tunnel                                 tunnel local ports to your balenaOS device
+  preload                                preload an app on a disk image (or Edison zip archive)
+  build                                  build a project locally
+  deploy                                 deploy a single image or a multicontainer project to a balena application
+  join                                   move a local device to an application on another balena server
+  leave                                  remove a local device from its balena application
 
 `;
 
 const ADDITIONAL_HELP = `
 ADDITIONAL COMMANDS
-  api-key generate <name>                generate a new balenaCloud API key
-  app create <name>                      create an application
-  app restart <name>                     restart an application
-  app rm <name>                          remove an application
+  api-key generate                       generate a new balenaCloud API key
+  app create                             create an application
+  app restart                            restart an application
+  app rm                                 remove an application
   config generate                        generate a config.json file
-  config inject <file>                   inject a configuration file into a device or OS image
+  config inject                          inject a configuration file into a device or OS image
   config read                            read the configuration of a device or OS image
   config reconfigure                     interactively reconfigure a device or OS image
-  config write <key> <value>             write a key-value pair to configuration of a device or OS image
+  config write                           write a key-value pair to configuration of a device or OS image
   device detect                          scan for balenaOS devices on your local network
-  device identify <uuid>                 identify a device
+  device identify                        identify a device
   device init                            initialise a device with balenaOS
   device list                            list all devices
-  device logs <device>                   show device logs
-  device move <uuid(s)>                  move one or more devices to another application
-  device os-update <uuid>                start a Host OS update for a device
-  device public-url <uuid>               get or manage the public URL for a device
-  device reboot <uuid>                   restart a device
-  device register <application>          register a device
-  device rename <uuid> [newName]         rename a device
-  device rm <uuid(s)>                    remove one or more devices
-  device shutdown <uuid>                 shutdown a device
-  device ssh <applicationOrDevice> [service]    SSH into the host or application container of a device
+  device logs                            show device logs
+  device move                            move one or more devices to another application
+  device os-update                       start a Host OS update for a device
+  device public-url                      get or manage the public URL for a device
+  device reboot                          restart a device
+  device register                        register a device
+  device rename                          rename a device
+  device rm                              remove one or more devices
+  device shutdown                        shutdown a device
+  device ssh                             SSH into the host or application container of a device
   devices supported                      list the supported device types (like 'raspberrypi3' or 'intel-nuc')
-  env add <name> [value]                 add env or config variable to application(s), device(s) or service(s)
+  env add                                add env or config variable to application(s), device(s) or service(s)
   env list                               list the environment or config variables of an application, device or service
-  env rename <name> <value>              change the value of a config or env var for an app, device or service
-  env rm <id>                            remove a config or env var from an application, device or service
-  ssh-key <id>                           display an SSH key
-  ssh-key add <name> [path]              add an SSH key to balenaCloud
+  env rename                             change the value of a config or env var for an app, device or service
+  env rm                                 remove a config or env var from an application, device or service
+  ssh-key                                display an SSH key
+  ssh-key add                            add an SSH key to balenaCloud
   ssh-key list                           list the SSH keys in balenaCloud
-  ssh-key rm <id>                        remove an SSH key from balenaCloud
-  local configure <target>               (Re)configure a balenaOS drive or image
-  local flash <image>                    flash an image to a drive
+  ssh-key rm                             remove an SSH key from balenaCloud
+  local configure                        (Re)configure a balenaOS drive or image
+  local flash                            flash an image to a drive
   logout                                 logout from balena
-  note <|note>                           set a device note
-  os build-config <image> <device-type>  build an OS config and save it to a JSON file
-  os configure <image>                   configure a previously downloaded balenaOS image
-  os download <type>                     download an unconfigured OS image
-  os initialize <image>                  initialize an os image for a device
-  os versions <type>                     show available balenaOS versions for the given device type
+  note                                   set a device note
+  os build-config                        build an OS config and save it to a JSON file
+  os configure                           configure a previously downloaded balenaOS image
+  os download                            download an unconfigured OS image
+  os initialize                          initialize an os image for a device
+  os versions                            show available balenaOS versions for the given device type
   settings                               print current settings
   tag list                               list all tags for a app, block, fleet, device or release
-  tag rm <tagKey>                        remove a tag from an app, block, fleet, device or release
-  tag set <tagKey> [value]               set a tag on an app, block, fleet, device or release
+  tag rm                                 remove a tag from an app, block, fleet, device or release
+  tag set                                set a tag on an app, block, fleet, device or release
   util available-drives                  list available drives
   version                                display version information for the balena CLI and/or Node.js
   whoami                                 display account information for current user
