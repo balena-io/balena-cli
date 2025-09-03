803030f360b5832552da45ca0ed37385d28abd1a
Matthew Yarmolinsky <yarmolinskymatthew@gmail.com>
2025-09-03 15:54:19 -0400

    Replace custom `version` command with `@oclif/plugin-version`
    This is a major because the `-a` flag is replace with `--verbose`, we drop the `-v` alternative, and we drop the `-j` shorthand of `--json`
    
    Change-type: major
    Fibery: https://balena.fibery.io/Work/Project/CLI-Replace-our-custom-version-command-with-oclif's-plugin-version-1819

--- src/preparser.ts
+++ src/preparser.ts
@@ -36,12 +36,8 @@ export async function preparseArgs(argv: string[]): Promise<string[]> {
 	const cmdSlice = argv.slice(2);
 
 	if (cmdSlice.length > 0) {
-		// convert 'balena --version' or 'balena -v' to 'balena version'
-		if (['--version', '-v'].includes(cmdSlice[0])) {
-			cmdSlice[0] = 'version';
-		}
 		// convert 'balena --help' to 'balena help'
-		else if ('--help' === cmdSlice[0]) {
+		if ('--help' === cmdSlice[0]) {
 			cmdSlice[0] = 'help';
 		}
 		// convert e.g. 'balena help env set' to 'balena env set --help'
