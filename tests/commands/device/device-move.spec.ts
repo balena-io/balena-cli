98fe5671e14272d3b3730c73f172083c340be082
Otavio Jacobi <otaviojacobi@gmail.com>
2025-10-18 09:09:07 -0300

    Move `device.tag.set` and `device-move` tests from nock to mockttp
    Change-type: patch

--- tests/commands/device/device-move.spec.ts
+++ tests/commands/device/device-move.spec.ts
@@ -16,20 +16,27 @@
  */
 
 import { expect } from 'chai';
-import { BalenaAPIMock } from '../../nock/balena-api-mock';
+
 import { cleanOutput, runCommand } from '../../helpers';
+import { MockHttpServer } from '../../mockserver';
 
 describe('balena device move', function () {
-	let api: BalenaAPIMock;
+	let api: MockHttpServer['api'];
+	let server: MockHttpServer;
 
-	beforeEach(() => {
-		api = new BalenaAPIMock();
-		api.expectGetWhoAmI({ optional: true, persist: true });
+	this.beforeAll(async () => {
+		server = new MockHttpServer();
+		api = server.api;
+		await server.start();
+		await api.expectGetWhoAmI({ optional: true, persist: true });
 	});
 
-	afterEach(() => {
-		// Check all expected api calls have been made and clean up.
-		api.done();
+	this.afterAll(async () => {
+		await server.stop();
+	});
+
+	this.afterEach(async () => {
+		await server.assertAllCalled();
 	});
 
 	it('should error if uuid not provided', async () => {
